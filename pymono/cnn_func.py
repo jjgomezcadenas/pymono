62e9961fc505b5aaeda749ea9fcf74ff51075855
jjgomezcadenas <jjgomezcadenas@gmail.com>
2023-12-16 12:04:08 +0100

    Add a new CNN for the case 3x3

--- pymono/cnn_func.py
+++ pymono/cnn_func.py
@@ -119,6 +119,71 @@ class CNN_basic(nn.Module):
         self.debug = False
 
         return x
+
+
+class CNN_3x3(nn.Module):
+    """
+    Defines a convolutional network with a basic architecture:
+    convolution (3x3) , reLU batch norm and MaxPool: (16,16,1) => (8,8,64)
+    convolution (2x2) , reLU batch norm and MaxPool: (8,8,64) => (4,4,128)
+    convolution (2x2) , reLU batch norm and MaxPool: (4,4,128) => (2,2,256)
+    convolution (1x1) , reLU batch norm and MaxPool: (2,2,256) => (1,1,512)
+    drop (optional) 
+    linear layer 512 => 3
+
+    Input to the network are the pixels of the pictures, output (x,y,z)
+
+    """
+    def __init__(self, chi=64, dropout=False, dropout_fraction=0.2):
+        super().__init__()
+        self.dropout = dropout
+        self.conv1 = nn.Conv2d(1, chi, 3, padding=1) 
+        self.bn1   = nn.BatchNorm2d(chi)
+        self.conv2 = nn.Conv2d(chi, chi*2, 3, padding=1)
+        self.bn2   = nn.BatchNorm2d(chi*2)
+        self.conv3 = nn.Conv2d(chi*2, chi*4, 2, padding=1)
+        self.bn3   = nn.BatchNorm2d(chi*4)
+        self.conv4 = nn.Conv2d(chi*4, chi*8, 2, padding=1)
+        self.bn4   = nn.BatchNorm2d(chi*8)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.fc0 = nn.Linear(chi*8, 3)
+        self.drop1 = nn.Dropout(p=dropout_fraction)
+        self.debug = True
+
+ 
+    def forward(self, x):
+
+        if(self.debug): print(f"input data shape =>{x.shape}")
+        # convolution (3x3) , reLU batch norm and MaxPool: (16,16,1) => (8,8,64)
+        if(self.debug): print(f"(16,16,1) => (8,8,64) =>{x.shape}")
+        x = self.pool(self.bn1(F.leaky_relu(self.conv1(x))))
+        
+        if(self.debug): print(f"(8,8,64) => (4,4,128) =>{x.shape}")
+        # convolution (2x2) , reLU batch norm and MaxPool: (8,8,64) => (4,4,128)
+        x = self.pool(self.bn2(F.leaky_relu(self.conv2(x))))
+        
+        if(self.debug): print(f"(4,4,128) => (2,2,256) =>{x.shape}")
+        # convolution (2x2) , reLU batch norm and MaxPool: (4,4,128) => (2,2,256)
+        x = self.pool(self.bn3(F.leaky_relu(self.conv3(x))))
+        
+        if(self.debug): print(f"(2,2,256) => (1,1,512) =>{x.shape}")
+        # convolution (2x2) , reLU batch norm and MaxPool: (2,2,256) => (1,1,512)
+        x = self.pool(self.bn4(F.leaky_relu(self.conv4(x))))
+        
+        #if(self.debug): print(f"(2,2,256) => (1,1,512) =>{x.shape}")
+        if(self.debug): print(f"(1,1,512) => (1,1,3) =>{x.shape}")
+        x = x.flatten(start_dim=1)
+        # Flatten
+        
+        if self.dropout: x = self.drop1(x)  # drop
+        
+        x = self.fc0(x)    # linear layer 512 => 3
+        
+        if(self.debug): print(x.shape)
+
+        self.debug = False
+
+        return x
     
 def train_cnn(train_loader, val_loader, model, optimizer, device, criterion, epochs=100):
     """
